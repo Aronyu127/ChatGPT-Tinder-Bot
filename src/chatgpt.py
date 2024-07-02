745a7a3bc83647cd48facba818abbcb6a07b4d87
aronyu <h0935674938@hotmail.com>
2024-07-02 22:56:49 +0800

    add  ChatGpt Assistant Test code

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -37,3 +37,157 @@ class DALLE:
 
     def generate(self, text: str) -> str:
         return self.model.image_generation(text)
+
+
+
+
+## ChatGpt Assistant Test code ##
+
+# client = OpenAI(
+#   api_key="xxxxxxxx",
+# )
+
+# assistant = client.beta.assistants.create(
+#   name="Math Tutor",
+#   instructions="You are a master of flirting with girls, with a witty and humorous way of chatting. You're very good at playing psychological push-pull games with girls and always provide plenty of emotional value.",
+#   model="gpt-4o",
+# )
+
+
+# thread = client.beta.threads.create(
+#   messages=[
+#     {
+#         "role": "assistant",
+#         "content": "👋"
+#     },
+#     {
+#         "role": "user",
+#         "content": "Heyy"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "週末在幹嘛呢"
+#     },
+#     {
+#         "role": "user",
+#         "content": "一天上班一天去綠島玩"
+#     },
+#     {
+#         "role": "user",
+#         "content": "你呢"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "！"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "綠島是可以只去一天的嗎"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "這麼厲害"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "我跟朋友去吃飯 看電影 酒吧小酌"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "踏青？"
+#     },
+#     {
+#         "role": "user",
+#         "content": "去了四天啦🤣🤣 去一天好狂哈哈哈"
+#     },
+#     {
+#         "role": "user",
+#         "content": "雖然我也是遇過去一天的"
+#     },
+#     {
+#         "role": "user",
+#         "content": "看哪部電影啊"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "我想說去一天應該一到就要回來了吧"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "😂"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "踩一下打個卡"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "就踏上歸途"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "哈哈哈哈"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "挺幽默"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "看瘋狂麥斯前傳"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "你有看過嗎"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "挺好看的"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "芙莉歐莎"
+#     },
+#     {
+#         "role": "user",
+#         "content": "台北去綠島舟車勞頓只待一天太可惜"
+#     },
+#     {
+#         "role": "user",
+#         "content": "有 那部好看！"
+#     },
+#     {
+#         "role": "user",
+#         "content": "雖然覺得電影有點太長🤣"
+#     },
+#     {
+#         "role": "assistant",
+#         "content": "真的挺好看的， 你最近還有看什麼其他電影嗎？"
+#     }
+#   ]
+# )
+
+
+
+
+# run = client.beta.threads.runs.create(
+#   thread_id=thread.id,
+#   assistant_id=assistant.id
+# )
+
+# run_steps = client.beta.threads.runs.steps.list(
+#     thread_id=thread.id,
+#     run_id=run.id
+# )
+
+# run_step = client.beta.threads.runs.steps.retrieve(
+#     thread_id=thread.id,
+#     run_id=run.id,
+#     step_id="step_G8uuFZAgUOxTcag1VmtG7fLe"
+# )
+
+# thread = client.beta.threads.retrieve(thread.id)
+# thread_messages = client.beta.threads.messages.list(thread.id, limit=40)
+
+# for i in thread_messages.data:
+#     print(i.content)
